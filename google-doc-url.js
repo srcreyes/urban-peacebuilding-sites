7858d98edfbddfd02ec05e1d8b4e5420560333e3
Sheryl Rose Reyes <52477189+srcreyes@users.noreply.github.com>
2023-02-27 22:53:50 +0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1L_JZp4VdJYfiQwUaQGUbVfo6w6vKQX20SGtMG6aVZJM/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyB_bow8xelOGIz31ecw-XQb7o3fK-i-WMk';
